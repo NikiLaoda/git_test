60d7bbf570cfd5721b012958dd00f00b977a5de0
Nikita Pustovalov <workpagelaoda@gmail.com>
2024-07-21 19:05:57 +0300

    Testing..

---
+++ test.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+
+const p = document.createElement('p');
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+
+const h3 = document.createElement('h3');
+h3.textContent = "I'm a blue h3!";
+h3.style.color = 'blue';
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div";
+const p2 = document.createElement('p2');
+p2.textContent = "ME TOO!";
+
+const div = document.createElement('div');
+div.style.border = '2px solid black';
+div.style.backgroundColor = 'pink';
+div.appendChild(h1);
+div.appendChild(p2);
+
+body.appendChild(p);
+body.appendChild(h3);
+body.appendChild(div);
